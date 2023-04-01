099cdded00fb36fe1586ebb5d571cebda81f9b56
mithrayls <mithray@protonmail.com>
2023-04-02 02:47:22 +1300

    second commit

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,18 @@
+<h1>Login</h1>
+<form method="POST">
+  <label>
+    <img src="user.svg">
+    <input type="text">
+  </label>
+  <label>
+    <img src="shield.svg">
+    <input type="password" id="input-password">
+  </label>
+  <button type="submit">Login</button>
+</form>
+<style>
+  form label{
+    width: 100%;
+    display: block;
+  }
+</style>
