ec3168e125d23992c3f02511d3dfcf36753bd6a9
Kimmy14315 <129602278+Kimmy14315@users.noreply.github.com>
2023-04-04 13:44:43 +0800

    Add files via upload

--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -1,6 +1,7 @@
+
 <h1>Register</h1>
 
-<form method="POST">
+<center><form method="POST">
 
   <label>
     <img src="user.svg">
@@ -20,6 +21,7 @@
   <button type="submit">Register</button>
 
 </form>
+</center>
 
 
 <style>
@@ -27,4 +29,29 @@
     width: 100%;
     display: block;
   }
+ form {
+      border: 5px solid #f1f1f1;
+      width: 50%;
+      }
+       input[type=text], input[type=password] {
+      width: 30%;
+      padding: 4px 4px;
+      margin: 8px 0;
+      display: inline-block;
+      border: 1px solid #ccc;
+      box-sizing: border-box;
+      }
+  button {
+      background-color: #8ebf42;
+      color: white;
+      padding: 4px 0;
+      margin: 10px 0;
+      border: none;
+      cursor: grabbing;
+      width: 20%;
+      }
+  h1 {
+      text-align:center;
+      fone-size:18;
+      }
 </style>
